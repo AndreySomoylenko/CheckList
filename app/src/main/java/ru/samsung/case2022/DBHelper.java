8fe0c6e0723a30e1a3c936eb819b342fe8209e43
Novip1906 <novipcs@gmail.com>
2023-02-06 20:44:00 +0300

    db classes added

---
+++ app/src/main/java/ru/samsung/case2022/DBHelper.java
@@ -0,0 +1,30 @@
+package ru.samsung.case2022;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "DB.db";
+    public static int DB_VERSION = 1;
+
+    public DBHelper(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE users (id INTEGER PRIMARY KEY, list TEXT)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS users");
+        onCreate(db);
+
+    }
+}
