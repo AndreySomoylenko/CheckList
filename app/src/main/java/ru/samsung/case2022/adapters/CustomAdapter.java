750901e15afabf73c199d053c264001867af02fd
GARGAMEL <kovalchuk.mitka@gmail.com>
2023-02-20 16:36:00 +0300

    Comments CustomAdapter

--- app/src/main/java/ru/samsung/case2022/adapters/CustomAdapter.java
+++ app/src/main/java/ru/samsung/case2022/adapters/CustomAdapter.java
@@ -22,21 +22,31 @@ import ru.samsung.case2022.R;
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
 
 
-    /**
-     *
-     */
+
     private final List<String> localDataSet;
     private final Context context;
 
     private final OnNoteListener mOnNoteListener;
 
 
+    /**
+     * it is the constructor of our adapter
+     * @param localDataSet is the user's list of products
+     * @param context is the application context
+     * @param onNoteListener is the api we use to catch click on element
+     */
     public CustomAdapter(List<String> localDataSet, Context context, OnNoteListener onNoteListener) {
         this.localDataSet = localDataSet;
         this.context = context;
         this.mOnNoteListener = onNoteListener;
     }
 
+    /**
+     * @param viewGroup The ViewGroup into which the new View will be added after it is bound to
+     *                  an adapter position.
+     * @param viewType  The view type of the new View.
+     * @return
+     */
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         // Create a new view, which defines the UI of the list item
@@ -47,22 +57,38 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
     }
 
 
+    /**
+     * @param viewHolder The ViewHolder which should be updated to represent the contents of the
+     *                   item at the given position in the data set.
+     * @param position   The position of the item within the adapter's data set.
+     */
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, final int position) {
 
         viewHolder.getTextView().setText(localDataSet.get(position));
     }
 
+
+    /**
+     * @return size of our list
+     */
     @Override
     public int getItemCount() {
         return localDataSet.size();
     }
 
+    /**
+     * it is constructor of our class
+     */
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView text;
 
         OnNoteListener onNoteListener;
 
+        /**
+         * @param itemView
+         * @param onNoteListener
+         */
         public ViewHolder(@NonNull View itemView, OnNoteListener onNoteListener) {
             super(itemView);
             text = itemView.findViewById(R.id.productName);
@@ -71,16 +97,25 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
             this.onNoteListener = onNoteListener;
         }
 
+        /**
+         * @return
+         */
         public TextView getTextView() {
             return text;
         }
 
+        /**
+         * @param view
+         */
         @Override
         public void onClick(View view) {
             onNoteListener.OnNoteClick(getAdapterPosition());
         }
     }
 
+    /**
+     *This interface is used to hear catch click on element
+     */
     public interface OnNoteListener {
         void OnNoteClick(int position);
     }
